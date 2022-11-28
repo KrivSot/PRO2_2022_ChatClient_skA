5ff3bfcf00c5171abd04fb046c6b060d09ff7b71
krivsot <krys396@seznam.cz>
2022-11-28 12:13:58 +0100

    TODO CVS file Operations, learn how this works and fix loggedUsers and message showing

--- src/models/chatClients/api/ApiChatClient.java
+++ src/models/chatClients/api/ApiChatClient.java
@@ -2,21 +2,26 @@ package models.chatClients.api;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 import models.Message;
 import models.chatClients.ChatClient;
 import models.chatClients.LocalDateTimeDeserializer;
 import models.chatClients.LocalDateTimeSerializer;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 //http://fimuhkpro22021.aspifyhost.cz/swagger/index.html
 public class ApiChatClient implements ChatClient {
     private String loggedUser;
@@ -33,19 +38,47 @@ public class ApiChatClient implements ChatClient {
     public ApiChatClient() {
         loggedUsers = new ArrayList<>();
         messages = new ArrayList<>();
-        gson = new GsonBuilder()
+        GsonBuilder gsonB = new GsonBuilder()
                 .excludeFieldsWithoutExposeAnnotation()
-                .setPrettyPrinting() // set user friendly file writing
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeSerializer())
-                .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeDeserializer())
-                .create();
+                .setPrettyPrinting(); // set user friendly file writing
+        gsonB.registerTypeAdapter(LocalDateTime.class,new LocalDateTimeSerializer());
+        gsonB.registerTypeAdapter(LocalDateTime.class,new LocalDateTimeDeserializer());
+        gson = gsonB.create();
+        Runnable refreshData = () ->{
+            Thread.currentThread().setName("RefreshData");
+            try{
+                while (true) {
+                    if(isAuthenticated()){
+                        refreshLoggedUsers();
+                        refreshMessages();
+                    }
+                    TimeUnit.SECONDS.sleep(2);
+                }
+            }catch(Exception e){}
+            refreshLoggedUsers();
+        };
+        Thread refreshDataThread = new Thread(refreshData);
+        refreshDataThread.start();
     }
 
     @Override
     public void sendMessage(String text) {
-        messages.add(new Message(loggedUser, text));
-        System.out.println("new message - " + text);
-        raiseEventMessagesChanged();
+        try{
+            SendMessageRequest msgRequest = new SendMessageRequest(token ,text);
+            String url = BASE_URL + "/api/Chat/SendMessage";
+            HttpPost post = new HttpPost(url);
+            StringEntity body = new StringEntity(gson.toJson(msgRequest),"utf-8");
+            body.setContentType("application/json");
+            post.setEntity(body);
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(post);
+
+            if(response.getStatusLine().getStatusCode() == 204){
+                refreshMessages();
+            }
+
+        }catch(Exception e){}
     }
 
     @Override
@@ -61,26 +94,21 @@ public class ApiChatClient implements ChatClient {
             CloseableHttpResponse response = httpClient.execute(post);
 
             if(response.getStatusLine().getStatusCode() == 200){
-                token = response.getEntity().toString();
+                token = EntityUtils.toString(response.getEntity());
                 token = token.replace("\"", "").trim();
 
                 loggedUser = userName;
-                raiseEventLoggedUsersChanged();
+                refreshLoggedUsers();
             }
         }catch (Exception e){
             e.printStackTrace();
         }
-        loggedUser = userName;
-        loggedUsers.add(userName);
-        addSystemMessages(Message.USER_LOGGED_IN, loggedUser);
-        System.out.println("user logged in: " + loggedUser);
-        raiseEventLoggedUsersChanged();
     }
 
     @Override
     public void logout() {
         try{
-            String url = BASE_URL + "/api/Chat/Login";
+            String url = BASE_URL + "/api/Chat/Logout";
             HttpPost post = new HttpPost(url);
             StringEntity body = new StringEntity("\""+token+"\"","utf-8");
             body.setContentType("application/json");
@@ -144,5 +172,51 @@ public class ApiChatClient implements ChatClient {
     private void addSystemMessages(int type, String author){
         messages.add(new Message(type,author));
         raiseEventMessagesChanged();
+    }
+
+    private void refreshLoggedUsers(){
+        try {
+            String url = BASE_URL + "/api/Chat/GetLoggedUsers";
+            HttpGet get = new HttpGet(url);
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(get);
+
+            if(response.getStatusLine().getStatusCode() == 200){
+                String resultJson = EntityUtils.toString(response.getEntity());
+
+                loggedUser = gson.fromJson(
+                        resultJson,
+                        new TypeToken<ArrayList<String>>(){}.getType()
+                );
+                raiseEventLoggedUsersChanged();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    private void refreshMessages(){
+        try {
+            String url = BASE_URL + "/api/Chat/GetMessages";
+            HttpGet get = new HttpGet(url);
+
+            CloseableHttpClient httpClient = HttpClients.createDefault();
+            CloseableHttpResponse response = httpClient.execute(get);
+
+            if(response.getStatusLine().getStatusCode() == 200){
+                String resultJson = EntityUtils.toString(response.getEntity());
+
+                messages = gson.fromJson(
+                        resultJson,
+                        new TypeToken<ArrayList<Message>>(){}.getType()
+                );
+                raiseEventMessagesChanged();
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
